5d964bff9a9d616f1b3a713f93e58e1abc38ae67
202310957 <snu11025@naver.com>
2025-12-06 20:36:36 +0900

    chore: main.py에서 if name == 'main' 블록 삭제

--- 003-ai-model-dashboard/001-vocab-ai/main.py
+++ 003-ai-model-dashboard/001-vocab-ai/main.py
@@ -25,6 +25,3 @@ async def extract_word(
 ):
     image = await image.read()
     return main_pipeline(image)
-
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
